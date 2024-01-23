0e51a664b3f3d07ce65bf7c9df7d7aaa3b620344
LostAndDead <jakemarsh9012@gmail.com>
2024-01-23 10:49:10 +0000

    Better looking login and registration

--- pages/login.php
+++ pages/login.php
@@ -50,10 +50,76 @@ function test_input($data) {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+<style>
+    .gradient-custom {
+        /* fallback for old browsers */
+        background: #6a11cb;
+
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
+
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
+    }
+
+    .form-label {
+        color: dimgray;
+        font-weight: bold;
+    }
+
+    .form-label-error {
+        color: red;
+        font-weight: bold;
+    }
+</style>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
-<h1> Login Please </h1>
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-3 text-center">
 
+                        <div class="mb-md-5 mt-md-4 pb-5">
+
+                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                            <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="email" id="typeEmailX" name="email" class="form-control form-control-lg" placeholder="name@example.com" value="<?php echo $email;?>" />
+                                    <?php
+                                    if($emailErr){
+                                        echo '<label class="form-label-error" for="typeEmailX">' . $emailErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typeEmailX">Email</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" placeholder="password123" value="<?php echo $password;?>" />
+                                    <?php
+                                    if($passwordErr){
+                                        echo '<label class="form-label-error" for="typePasswordX">' . $passwordErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typePasswordX">Password</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit" name="submit">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     E-mail: <input type="text" name="email" value="<?php echo $email;?>">
     <span class="error">* <?php echo $emailErr;?></span>
@@ -65,3 +131,4 @@ function test_input($data) {
     <br>
     <span class="error"><?php echo $msg;?></span>
 </form>
+-->
--- pages/register.php
+++ pages/register.php
@@ -3,7 +3,7 @@
 require "../utils/sql.php";
 
 $email = $username = $password = $passwordConfirm = "";
-$err = $usernameErr = $emailErr = $passwordErr = "";
+$msg = $usernameErr = $emailErr = $passwordErr = $passwordConfirmErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["username"])) {
@@ -30,25 +30,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password = test_input($_POST["password"]);
         $passwordConfirm = test_input($_POST["password_confirm"]);
         if ($password != $passwordConfirm) {
-            $passwordErr = "Passwords Dont Match";
+            $passwordConfirmErr = "Passwords Dont Match";
         }
     }
 
-    if(!$err && !$usernameErr && !$emailErr && !$passwordErr){
+    if(!$msg && !$usernameErr && !$emailErr && !$passwordErr && !$passwordConfirmErr){
         $passwordHash = password_hash($password, PASSWORD_DEFAULT);
         $result = createUser($username, $email, $passwordHash, false);
         switch ($result){
             case "Duplicate": {
-                $err = "Email or username is already in use";
+                $msg = "Email or username is already in use";
                 break;
             }
             case "Worked": {
-                $err = "User created, you will be redirected to login.";
+                $msg = "User created, you will be redirected to login.";
                 header( "refresh:3;url=login.php?email=" . $email );
                 break;
             }
             case "shrug": {
-                $err = "Something went wrong, I dont know how. This should never happen";
+                $msg = "Something went wrong, I dont know how. This should never happen";
                 break;
             }
         }
@@ -68,9 +68,109 @@ function test_input($data) {
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
+<style>
+    .gradient-custom {
+        /* fallback for old browsers */
+        background: #6a11cb;
 
-<h1> Register Account </h1>
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
+
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
+    }
+
+    .form-label {
+        color: dimgray;
+        font-weight: bold;
+    }
+
+    .form-label-error {
+        color: red;
+        font-weight: bold;
+    }
+</style>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-3 text-center">
+
+                        <div class="mb-md-5 mt-md-4 pb-5">
+
+                            <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+                            <p class="text-white-50 mb-5">Please enter your details to register.</p>
+                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="text" id="typeUsernameX" name="username" class="form-control form-control-lg" placeholder="username" value="<?php echo $username;?>" />
+                                    <?php
+                                    if($usernameErr){
+                                        echo '<label class="form-label-error" for="typeUsernameX">' . $usernameErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typeUsernameX">Username</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="email" id="typeEmailX" name="email" class="form-control form-control-lg" placeholder="name@example.com" value="<?php echo $email;?>" />
+                                    <?php
+                                    if($emailErr){
+                                        echo '<label class="form-label-error" for="typeEmailX">' . $emailErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typeEmailX">Email</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" placeholder="password123" value="<?php echo $password;?>" />
+                                    <?php
+                                    if($passwordErr){
+                                        echo '<label class="form-label-error" for="typePasswordX">' . $passwordErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typePasswordX">Password</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <div class="form-outline form-white form-floating mb-4">
+                                    <input type="password" id="typePasswordConfirmX" name="password_confirm" class="form-control form-control-lg" placeholder="password123" value="<?php echo $passwordConfirm;?>" />
+                                    <?php
+                                    if($passwordConfirmErr){
+                                        echo '<label class="form-label-error" for="typePasswordConfirmX">' . $passwordConfirmErr . '</label>';
+                                    }else {
+                                        echo '<label class="form-label" for="typePasswordConfirmX">Password Confirmation</label>';
+                                    }
+                                    ?>
+                                </div>
+
+                                <?php
+                                if($msg){
+                                    echo '<div>';
+                                    echo '<p class="mb-0">' . $msg . '</p>';
+                                    echo '<div>';
+                                }
+                                ?>
+
+
+
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit" name="submit">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     Username: <input type="text" name="username" value="<?php echo $username;?>">
@@ -86,5 +186,6 @@ function test_input($data) {
     <br><br>
     <input type="submit" name="submit" value="Submit">
     <br>
-    <span class="error"><?php echo $err;?></span>
+    <span class="error"><?php echo $msg;?></span>
 </form>
+-->
